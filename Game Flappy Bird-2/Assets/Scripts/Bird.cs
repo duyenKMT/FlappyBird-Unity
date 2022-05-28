196d7d109c8aaf897322d3bf916bd193807c47a7
Duyên Trần Thị <Duyen@gmail.com>
2022-05-28 22:52:33 +0700

    game flappy bird

---
+++ Game Flappy Bird-2/Assets/Scripts/Bird.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D rigibody;
+    public float jumpForce;
+    private bool levelStart; //trạng thái chơi
+    public GameObject gameController;
+
+    public GameObject message;
+
+    private int score;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        SoundController.instance.PlayThisSound("GameMusic", 0.2f);
+        rigibody =this.gameObject.GetComponent <Rigidbody2D>();
+        levelStart = false;
+        rigibody.gravityScale = 0;
+        score = 0;
+        scoreText.text = score.ToString();
+        message.GetComponent<SpriteRenderer>().enabled = true;
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //Kiểm tra Space có bấm không
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SoundController.instance.PlayThisSound("wing", 0.5f);
+            if (levelStart == false)
+            {
+                levelStart = true;
+                rigibody.gravityScale = 6;
+                gameController.GetComponent<_pipeGenerator>().enabelGeneratePipe = true;
+                message.GetComponent<SpriteRenderer>().enabled = false;
+            
+            }
+            BirdMoveUp();
+        }
+    }
+    //Hàm làm cho chim bay lên 1 khoảng
+    private void BirdMoveUp()
+    {
+        rigibody.velocity = Vector2.up * jumpForce;
+    }
+
+// Xử lý va chạm
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SoundController.instance.PlayThisSound("hit", 0.5f); ///Lôiix
+        ReloadScene();
+        score = 0;
+        scoreText.text = score.ToString();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collection)
+    {
+        SoundController.instance.PlayThisSound("point", 0.5f);
+        score += 1;
+        scoreText.text = score.ToString();
+    }
+
+    public void ReloadScene()
+    {
+
+        SceneManager.LoadScene("GamePlay");
+    }
+}
---
+++ Game Flappy Bird-2/Assets/Scripts/_pipeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _pipeGenerator : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    private float countdown; //biến đếm ngược
+    public float timeDuration;
+    public bool enabelGeneratePipe; //cho phep sinh ong
+
+    //Hàm này đc gọi đúng 1 lần khi script đc tạo ra
+    public void Awake()
+    {
+        countdown = 0;
+        enabelGeneratePipe = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (enabelGeneratePipe == true)
+        {
+            countdown -= Time.deltaTime;    //Mỗi frame thì countdown-=1/FPS
+                                            //VD: 30FPS--> 30 khung hình/1s
+                                            //==> Mỗi frame countdown giảm đi 1/30s. Mà 1s gồm 30frame --> countdown giảm đúng 1 đvi
+            if (countdown <= 0)//Sinh ra ống
+            {
+                //Instantiate: Khởi tạo ra 1 GameObj
+                Instantiate(pipePrefab, new Vector3(10, Random.Range(-1.2f, 2.1f), 0), Quaternion.identity);
+                countdown = timeDuration;
+            }
+        }
+    }
+}
